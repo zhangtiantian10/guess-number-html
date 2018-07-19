010af5d0fa57d065d0370a5e82a2f485f4b17688
Tian Zhang <tizhang@CNtizhang.local>
2018-07-19 14:36:46 +0800

    write guess number game test when answer and input is 1 2 3 4
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

---
+++ guess-number-game.test.js
@@ -0,0 +1,7 @@
+const GuessNumberGame = require('./guess-number-game');
+
+describe('GuessNumberGame', function() {
+    it('should return 4A0B for input 1 2 3 4 when answer is 1 2 3 4', function () {
+        expect(new GuessNumberGame().start('1 2 3 4')).toEqual('4A0B');
+    });
+})

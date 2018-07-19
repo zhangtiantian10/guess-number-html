3f6b884c37d895b5c157b638f0c60954551886ec
Tian Zhang <tizhang@CNtizhang.local>
2018-07-19 22:07:46 +0800

    achieve Generator generate random 4 digits
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

---
+++ generator.js
@@ -0,0 +1,19 @@
+class Generator {
+    constructor() {
+
+    }
+
+    generate() {
+        const answers = [];
+        while (answers.length < 4) {
+            const random = Math.floor(Math.random() * 10);
+            if (!answers.includes(random)) {
+                answers.push(random);
+            }
+        }
+
+        return answers.join(' ');
+    }
+}
+
+module.exports = Generator;

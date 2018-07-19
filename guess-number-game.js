31c8f0c656c96e2872dd80a4fe794c5964c08c97
Tian Zhang <tizhang@CNtizhang.local>
2018-07-19 17:01:03 +0800

    refacte guessNumber
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- guess-number-game.js
+++ guess-number-game.js
@@ -3,6 +3,12 @@ class GuessNumberGame {
         this.answer = '1 2 3 4';
     }
 
+    test(input) {
+        const {position, includeNumber} = this.compare(input)
+
+        return `${position}A${includeNumber - position}B`;
+    }
+
     compare(input) {
         const inputs = input.split(' ');
         const answers = this.answer.split(' ');
@@ -10,13 +16,7 @@ class GuessNumberGame {
         const position = inputs.filter((item, index) => answers[index] === item).length;
         const includeNumber = inputs.filter(item => answers.includes(item)).length
 
-        return `${position}A${includeNumber - position}B`;
-    }
-
-    checkInput(string) {
-        const array = string.split(' ');
-        const charCodes = array.filter(a => a.charCodeAt(0) > 57 || a.charCodeAt(0) < 48)
-        return array.length === 4 && charCodes.length === 0;
+        return {position, includeNumber};
     }
 }
 
--- guess-number-game.test.js
+++ guess-number-game.test.js
@@ -2,26 +2,22 @@ const GuessNumberGame = require('./guess-number-game');
 
 describe('GuessNumberGame', function() {
     it('should return 4A0B for checkInput 1 2 3 4 when answer is 1 2 3 4', function () {
-        expect(new GuessNumberGame().compare('1 2 3 4')).toEqual('4A0B');
+        expect(new GuessNumberGame().test('1 2 3 4')).toEqual('4A0B');
     });
 
     it('should return 3A0B for checkInput 1 2 3 5 when answer is 1 2 3 4', function () {
-        expect(new GuessNumberGame().compare('1 2 3 5')).toEqual('3A0B')
+        expect(new GuessNumberGame().test('1 2 3 5')).toEqual('3A0B')
     });
 
     it('should return 0A3B for checkInput 5 1 2 3 when answer is 1 2 3 4', function () {
-        expect(new GuessNumberGame().compare('5 1 2 3')).toEqual('0A3B');
+        expect(new GuessNumberGame().test('5 1 2 3')).toEqual('0A3B');
     });
 
     it('should return 2A2B for checkInput 1 2 4 3 when answer is 1 2 3 4', function () {
-        expect(new GuessNumberGame().compare('1 2 4 3')).toEqual('2A2B');
+        expect(new GuessNumberGame().test('1 2 4 3')).toEqual('2A2B');
     });
 
-    it('should return true when checkInput is 4 digit number ', function () {
-        expect(new GuessNumberGame().checkInput('1 2 3 4')).toEqual(true);
-    });
+    it('should console game over info when input 6 times', function () {
 
-    it('should return false when checkInput include char', function () {
-        expect(new GuessNumberGame().checkInput('1 a 3 4')).toEqual(false);
     });
 })

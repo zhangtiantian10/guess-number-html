8f2eb37288655b9fd726d5d6d0208eddb3e5a479
Tian Zhang <tizhang@CNtizhang.local>
2018-07-19 21:57:25 +0800

    achieve generateAnswer
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- guess-number-game.js
+++ guess-number-game.js
@@ -1,6 +1,6 @@
 class GuessNumberGame {
     constructor() {
-        this.answer = '1 2 3 4';
+        this.answer = this.generateAnswer();
     }
 
     test(input) {
@@ -12,11 +12,22 @@ class GuessNumberGame {
     compare(input) {
         const inputs = input.split(' ');
         const answers = this.answer.split(' ');
-
         const position = inputs.filter((item, index) => answers[index] === item).length;
         const includeNumber = inputs.filter(item => answers.includes(item)).length
 
         return {position, includeNumber};
+    }
+
+    generateAnswer() {
+        const answers = [];
+        while (answers.length < 4) {
+            const random = Math.floor(Math.random() * 10);
+            if (!answers.includes(random)) {
+                answers.push(random);
+            }
+        }
+
+        return answers.join(' ');
     }
 }
 
